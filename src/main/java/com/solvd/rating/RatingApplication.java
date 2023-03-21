1fbdcbae88264a9a9d2edffa241c400753fe4b80
Hanna <theannalios@gmail.com>
2023-03-21 10:34:21 +0300

    Simple app is created

---
+++ src/main/java/com/solvd/rating/RatingApplication.java
@@ -0,0 +1,15 @@
+package com.solvd.rating;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class RatingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RatingApplication.class, args);
+	}
+
+}
